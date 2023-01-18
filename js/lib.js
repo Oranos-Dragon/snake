4bbbd50ffb8a28e262386ef36bd2f6ee0f282082
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2023-01-18 19:07:21 +0100

    snake body

--- js/lib.js
+++ js/lib.js
@@ -1,3 +1,131 @@
 function gameOver() {
     console.log("L bozo")
 }
+
+function snakeMoving() {
+    if (direction == 1) {
+        up();
+    }
+    else if (direction == 2) {
+        right();
+    }
+    else if (direction == 3) {
+        down();
+    }
+    else if (direction == 4) {
+        left();
+    }
+}
+
+function apple() {
+    applePosition = Math.floor(Math.random()*100)
+    while (applePosition == position || snakeBody.includes(applePosition)) {
+        apple()
+    }
+    boxes[applePosition].innerHTML = '<img src="/img/apple.png" alt="">';
+    grid[applePosition - 1] = 2
+}
+
+function eatApple() {
+    console.log("apple eaten")
+    snakeBody.push(snakeBody[snakeBody.length-1])
+    apple()
+
+}
+
+function up() {
+    if (position - 10 < 0) {
+        gameOver()
+    }    
+    else {
+        grid[position] = 0;
+        boxes[position].innerHTML = " "
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = " ";
+        }
+        snakeBody.pop();
+        snakeBody.unshift(position);
+        position -= 10;
+        boxes[position].innerHTML = '<img src="/img/snake_head_up.png" alt="">';
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
+        }
+        if (applePosition == position) {
+            eatApple()
+        };
+        grid[position] = 1;
+        direction = 1;
+    }
+}
+function right() {
+    if (position == 9 || position == 19 || position == 29 || position == 39 || position == 49 || position == 59 || position == 69 || position == 79 || position == 89 || position == 99){
+        gameOver()
+    }
+    else {
+        grid[position] = 0;
+        boxes[position].innerHTML = " ";
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = " ";
+        }
+        snakeBody.pop();
+        snakeBody.unshift(position);
+        position++;
+        boxes[position].innerHTML = '<img src="/img/snake_head_right.png" alt="">';
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
+        }
+        if (applePosition == position) {
+            eatApple()
+        };
+        grid[position] = 1 ; 
+        direction = 2;
+    }
+}
+function down() {
+    if (position + 10 > 99) {
+        gameOver()
+    } 
+    else {
+        grid[position] = 0;
+        boxes[position].innerHTML = " ";
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = " ";
+        }
+        snakeBody.pop();
+        snakeBody.unshift(position);
+        position += 10;
+        boxes[position].innerHTML = '<img src="/img/snake_head_down.png" alt="">';
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
+        }
+        if (applePosition == position) {
+            eatApple()
+        };
+        grid[position] = 1;
+        direction = 3;
+    }
+}
+function left() {
+    if (position == 0 || position == 10 || position == 20 || position == 30 || position == 40 || position == 50 || position == 60 || position == 70 || position == 80 || position == 90){
+        gameOver()
+    }
+    else {
+        grid[position] = 0;
+        boxes[position].innerHTML = " ";
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = " ";
+        }
+        snakeBody.pop();
+        snakeBody.unshift(position);
+        position--;
+        boxes[position].innerHTML = '<img src="/img/snake_head_left.png" alt="">';
+        for(i=0;i<snakeBody.length;i++) {
+            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
+        }
+        if (applePosition == position) {
+            eatApple()
+        };
+        grid[position] = 1;
+        direction = 4
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-const qs = document.querySelector;
-
 const boxes = document.querySelectorAll('.box');
-
+let start = false;
+let pause = false;
+let snakeBody = [21];
+let score = 0;
+let highscore = 0;
+let direction = 2;
 let grid = 
 [0,0,0,0,0,0,0,0,0,0,
-0,0,1,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,
 0,0,0,0,0,0,0,0,0,0,
@@ -16,41 +19,34 @@ let grid =
 ];
 
 
+let applePosition = 54;
+grid[applePosition] = 2;
 let position = 22;
 
 document.addEventListener('keydown', function(event) {
     console.log(event.key);
-    if (event.key === "w") {
-        if (position == 0, position == 1, position == 2, position == 3, position == 4, position == 5, position == 6, position == 7, position == 8, position ==9) {
-            gameOver()
+    if (event.key === "w" && direction != 3) {
+        direction = 1 ;
+    }
+    if (event.key === "a" && direction != 2) {
+        direction = 4;
         }
-        else {
-            grid[position] = 0;
-            boxes[position].innerHTML = " "
-            position -= 10;
-            boxes[position].innerHTML = '<img src="/img/snake_head_up.png" alt="">'
-            grid[position] = 1
+    if (event.key === "s" && direction != 1) {
+        direction = 3;
+    }
+    if (event.key === "d" && direction != 4) {
+        direction = 2;
+    }
+    if (event.key === " ") {
+        if (start == true && pause == false) {
+            pause = true;
+            clearInterval(snakeMove)
+            console.log("paused")
         }
-    }
-    if (event.key === "a") {
-        grid[position] = 0;
-        boxes[position].innerHTML = " "
-        position--;
-        boxes[position].innerHTML = '<img src="/img/snake_head_left.png" alt="">'
-        grid[position] = 1
-    }
-    if (event.key === "s") {
-        grid[position] = 0;
-        boxes[position].innerHTML = " "
-        position += 10;
-        boxes[position].innerHTML = '<img src="/img/snake_head_down.png" alt="">'
-        grid[position] = 1
-    }
-    if (event.key === "d") {
-        grid[position] = 0;
-        boxes[position].innerHTML = " "
-        position++;
-        boxes[position].innerHTML = '<img src="/img/snake_head_right.png" alt="">'
-        grid[position] = 1  
+        else if (start == false || pause == true) {
+            snakeMove = setInterval(function () {snakeMoving()}, 200);
+            start = true;
+            pause = false;
+        }
     }
 })
