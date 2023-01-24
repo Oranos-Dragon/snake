5ae6544dbaaae0cbe609b967ec0ca1d6bee0b30e
Oranos-Dragon <302164886@student.rocmondriaan.nl>
2023-01-24 12:13:37 +0100

    clean
    cleaned code

--- js/lib.js
+++ js/lib.js
@@ -1,14 +1,33 @@
-function gameOver() {
-    clearInterval(snakeMove);
+function gameOver() {//wordt uitgevoert al ga je dood
+    clearInterval(snakeMove);//stopt de slang met bewegen
     restart.style.visibility = "visible";
     gameover = true;
+    boom.play();
 }
 
 function restartButton() {
-    location.reload()
+    location.reload();//reload de site
 }
 
-function snakeMoving() {
+function snakeRemove() {
+    boxes[position].innerHTML = " ";
+        for(i=0;i<snakeBody.length;i++) {//haalt het lichaam helemaal weg
+            boxes[snakeBody[i]].innerHTML = " ";
+        }
+        snakeBody.pop();//haalt het laatste deel van de slang weg in de array
+        snakeBody.unshift(position);//zet een nieuw getal in de array op de positie waar het hoofd eerst was
+}
+
+function snakePlace() {
+    if (snakeBody.includes(position)) {//zorgt ervoor dat als je in jezelf gaat je dan dood gaat
+        gameOver();
+    }
+    for(i=0;i<snakeBody.length;i++) {//zet het lichaam wee terug
+        boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
+    }
+}
+
+function snakeMoving() {//wordt elke 200ms uitgevoerd om de slang te bewegen
     if (direction == 1) {
         up();
     }
@@ -24,130 +43,88 @@ function snakeMoving() {
 }
 
 function apple() {
-    applePosition = Math.floor(Math.random()*100);
+    applePosition = Math.floor(Math.random()*100);//genereerd een willekeurig getal voor de appel
     while (applePosition == position || snakeBody.includes(applePosition)) {
         apple();
     }
-    boxes[applePosition].innerHTML = '<img src="/img/apple.png" alt="">';
-    
+    boxes[applePosition].innerHTML = '<img src="/img/apple.png" class="apple" alt="">';//zet de appel neer 
 }
 
 function eatApple() {
     console.log("apple eaten");
-    snakeBody.push(snakeBody[snakeBody.length-1]);
+    snakeBody.push(snakeBody[snakeBody.length-1]);//1 vakje word toegevoegd aan de slang
     apple();
     scoreUpdate()
+    slurp.play();
 }
 
 function up() {
-    if (position - 10 < 0) {
-        gameOver()
+    if (position - 10 < 0) {//checkt of het vakje waar hij naartoegaat wel bestaat en als hij dat niet gaat de slang dood
+        gameOver();
     }    
     else {
-        boxes[position].innerHTML = " "
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = " ";
-        }
-        snakeBody.pop();
-        snakeBody.unshift(position);
+        snakeRemove();
         position -= 10;
-        if (snakeBody.includes(position)) {
-            gameOver()
-        }
-        boxes[position].innerHTML = '<img src="/img/snake_head_up.png" alt="">';
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
-        }
+        snakePlace();
+        boxes[position].innerHTML = '<img src="/img/snake_head_up.png" alt="">';//plaatst hoofd
         if (applePosition == position) {
-            eatApple()
+            eatApple();
         };
         direction = 1;
     }
 }
 function right() {
     if (position == 9 || position == 19 || position == 29 || position == 39 || position == 49 || position == 59 || position == 69 || position == 79 || position == 89 || position == 99){
-        gameOver()
+        gameOver();
     }
     else {
-        
-        boxes[position].innerHTML = " ";
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = " ";
-        }
-        snakeBody.pop();
-        snakeBody.unshift(position);
+        snakeRemove();
         position++;
-        if (snakeBody.includes(position)) {
-            gameOver()
-        }
-        boxes[position].innerHTML = '<img src="/img/snake_head_right.png" alt="">';
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
-        }
+        snakePlace();
+        boxes[position].innerHTML = '<img src="/img/snake_head_right.png" alt="">';//plaatst hoofd
         if (applePosition == position) {
-            eatApple()
+            eatApple();
         };
         direction = 2;
     }
 }
 function down() {
-    if (position + 10 > 99) {
-        gameOver()
+    if (position + 10 > 99) {//checkt of het vakje waar hij naartoegaat wel bestaat en als hij dat niet gaat de slang dood
+        gameOver();
     } 
     else {
-        
-        boxes[position].innerHTML = " ";
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = " ";
-        }
-        snakeBody.pop();
-        snakeBody.unshift(position);
+        snakeRemove();
         position += 10;
-        if (snakeBody.includes(position)) {
-            gameOver()
-        }
-        boxes[position].innerHTML = '<img src="/img/snake_head_down.png" alt="">';
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
-        }
+        snakePlace();
+        boxes[position].innerHTML = '<img src="/img/snake_head_down.png" alt="">';//plaatst hoofd
         if (applePosition == position) {
-            eatApple()
-        };
+            eatApple();
+        }
         direction = 3;
     }
 }
 function left() {
     if (position == 0 || position == 10 || position == 20 || position == 30 || position == 40 || position == 50 || position == 60 || position == 70 || position == 80 || position == 90){
-        gameOver()
+        gameOver();
     }
     else {
-        boxes[position].innerHTML = " ";
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = " ";
-        }
-        snakeBody.pop();
-        snakeBody.unshift(position);
+        snakeRemove();
         position--;
-        if (snakeBody.includes(position)) {
-            gameOver()
-        }
-        boxes[position].innerHTML = '<img src="/img/snake_head_left.png" alt="">';
-        for(i=0;i<snakeBody.length;i++) {
-            boxes[snakeBody[i]].innerHTML = '<img src="/img/snake_body.png" alt="">';
-        }
+        snakePlace();
+        boxes[position].innerHTML = '<img src="/img/snake_head_left.png" alt="">';//plaatst hoofd
         if (applePosition == position) {
-            eatApple()
-        };
-        direction = 4
+            eatApple();
+        }
+        direction = 4;
     }
 }
 
 function scoreUpdate() {
     score++;
-    scoreDisplay.innerHTML = score;
-    if (score > highscore) {
+    scoreDisplay.innerHTML = score;//laat de score zien
+    if (score > highscore) {//als de score groter is dan de highscore maakt het de score de nieuwe highscore en slaat het op in local storage
         highscore = score;
         highscoreDisplay.innerHTML = highscore;
-        localStorage.setItem("highscore", highscore)
+        localStorage.setItem("highscore", highscore);
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+//consts
 const boxes = document.querySelectorAll(".box");
 const scoreDisplay = document.querySelector(".score");
 const highscoreDisplay = document.querySelector(".highscore");
 const restart = document.querySelector(".restart");
 
+
+//audio
+const slurp = new Audio("img/slorp.mp3");
+const boom = new Audio("img/boom.mp3");
+const swipe = new Audio("img/swipe.mp3");
+const swipe2 = new Audio("img/swipe2.mp3");
+
+//lets
 let start = false;
 let pause = false;
 let gameover = false;
@@ -13,39 +22,42 @@ let direction = 2;
 let applePosition = 54;
 let position = 22;
 
-restart.addEventListener("click", restartButton)
-document.addEventListener("keydown", function(event) {
-    console.log(event.key);
-    if (event.key === "w" && direction != 3) {
-        direction = 1 ;
+restart.addEventListener("click", restartButton)//restart knop
+document.addEventListener("keydown", function(event) {//alle knoppen
+    if (event.key === "w" && direction != 3 || event.key === "ArrowUp" && direction != 3) {//omhoog
+        direction = 1;
+        swipe.play();
     }
-    if (event.key === "a" && direction != 2) {
+    if (event.key === "a"  && direction != 2 || event.key === "ArrowLeft" && direction != 2) {//naar links
         direction = 4;
+        swipe2.play();
         }
-    if (event.key === "s" && direction != 1) {
+    if (event.key === "s" && direction != 1 || event.key === "ArrowDown" && direction != 1) {//naar beneden
         direction = 3;
+        swipe.play();
     }
-    if (event.key === "d" && direction != 4) {
+    if (event.key === "d" && direction != 4 || event.key === "ArrowRight" && direction != 4) {//naar rechts
         direction = 2;
+        swipe2.play();
     }
-    if (event.key === " ") {
+    if (event.key === " ") {//als je op space drukt gaat hij op pauze
         if (gameover == false) {
             if (start == true && pause == false) {
                 pause = true;
-                clearInterval(snakeMove)
-                console.log("paused")
+                clearInterval(snakeMove);//stopt de slang met bewegen
+                console.log("paused");
             }
             else if (start == false || pause == true) {
-                snakeMove = setInterval(function () {snakeMoving()}, 200);
+                snakeMove = setInterval(function () {snakeMoving()}, 200);//start de slang met bewegen
                 start = true;
                 pause = false;
             }
         }
     }
     if (event.key === "r") {
-        restartButton()
+        restartButton();//reload de pagina
     }
 })
 
-highscore = localStorage.getItem("highscore");
-highscoreDisplay.innerHTML = parseInt(highscore)
+highscore = localStorage.getItem("highscore");//slaat de score op
+highscoreDisplay.innerHTML = parseInt(highscore);
